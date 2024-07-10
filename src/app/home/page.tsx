637940bd7afa2aec41e72decd395ed826fdbde1c
Luis R <luiscr1104@gmail.com>
2024-07-09 20:18:46 -0600

    Agrego git remoto

---
+++ src/app/home/page.tsx
@@ -0,0 +1,230 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Check, Star } from "lucide-react";
+import Image from "next/image";
+
+export default function HomePage() {
+  return (
+    <div className="bg-slate-50">
+      <section>
+        <MaxWidthWrapper
+          className="pb-24 pt-10 lg:grid 
+          lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 
+          lg:pt-24 xl:pt-32 lg:pb-52"
+        >
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div
+              className="relative mx-auto 
+              text-center lg:text-left flex flex-col 
+              items-center lg:items-start"
+            >
+              <div
+                className="absolute w-28 left-0 -top-20 hidden
+                lg:block"
+              >
+                <Image
+                  src="/snake-1.png"
+                  alt="logo-cobra"
+                  width={100}
+                  height={100}
+                  className="w-full"
+                />
+              </div>
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Descubre tu Terreno Ideal en{" "}
+                <span className="bg-custom-violet px-2 text-white">
+                  La Fortuna
+                </span>
+              </h1>
+
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                Asegura tu futuro invirtiendo en un terreno con{" "}
+                <span className="font-semibold">InvierteCR</span>. Somos líderes
+                en el mercado, garantizando la mejor inversión para ti.
+              </p>
+
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <li className="flex gap-1.5 items-center text-left">
+                  <Check className="h-5 w-5 shrink-0 text-custom-violet" />
+                  Financiamiento integral: 100% de su Quinta, Lote o Terreno.
+                </li>
+                <li className="flex gap-1.5 items-center text-left">
+                  <Check className="h-5 w-5 shrink-0 text-custom-violet" />
+                  Sin complicaciones: Sin prima, fiador ni estudios crediticios.
+                </li>
+                <li className="flex gap-1.5 items-center text-left">
+                  <Check className="h-5 w-5 shrink-0 text-custom-violet" />
+                  Flexibilidad de pago: Abonos extraordinarios sin penalidades.
+                </li>
+              </ul>
+
+              <div
+                className="mt-12 flex flex-col sm:flex-row 
+                items-center sm:items-start gap-5"
+              >
+                <div className="flex -space-x-4">
+                  <Image
+                    width={100}
+                    height={100}
+                    className="inline-block object-cover h-10 w-10 rounded-full
+                    ring-2 ring-slate-100"
+                    src="/users/user-1.png"
+                    alt="user image"
+                  />
+                  <Image
+                    width={100}
+                    height={100}
+                    className="inline-block h-10 w-10 rounded-full
+                    ring-2 ring-slate-100"
+                    src="/users/user-2.png"
+                    alt="user2 image"
+                  />
+                  <Image
+                    width={100}
+                    height={100}
+                    className="inline-block h-10 w-10 rounded-full
+                    ring-2 ring-slate-100"
+                    src="/users/user-3.png"
+                    alt="user3 image"
+                  />
+                  <Image
+                    width={100}
+                    height={100}
+                    className="inline-block h-10 w-10 rounded-full
+                    ring-2 ring-slate-100"
+                    src="/users/user-4.jpg"
+                    alt="user4 image"
+                  />
+                  <Image
+                    width={100}
+                    height={100}
+                    className="inline-block h-10 w-10 rounded-full
+                    ring-2 ring-slate-100"
+                    src="/users/user-5.jpg"
+                    alt="user5 image"
+                  />
+                </div>
+                <div className="flex flex-col justify-between items-center sm:items-start">
+                  <div className="flex gap-0.5">
+                    <Star className="h-4 w-4 text-custom-violet fill-custom-violet" />
+                    <Star className="h-4 w-4 text-custom-violet fill-custom-violet" />
+                    <Star className="h-4 w-4 text-custom-violet fill-custom-violet" />
+                    <Star className="h-4 w-4 text-custom-violet fill-custom-violet" />
+                    <Star className="h-4 w-4 text-custom-violet fill-custom-violet" />
+                  </div>
+                  <p>
+                    <span className="font-semibold">1.250</span> happy customers
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            className="col-span-full lg:col-span-1 w-full flex justify-center px-8
+              sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit"
+          >
+            <div className="relative md:max-w-xl">
+              <Image
+                src={"/your-image.png"}
+                alt="your image"
+                width={250}
+                height={250}
+                className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden 
+                  sm:block lg:hidden xl:block"
+              />
+              <Image
+                src={"/line.png"}
+                alt=""
+                width={250}
+                height={250}
+                className="absolute w-20 -left-6 -bottom-6 select-none"
+              />
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+
+      {/*  Value proposition section*/}
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              What our custumers say
+            </h2>
+          </div>
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap0.5 mb-2">
+                <Star className="h-5 w-5 text-custom-violet fill-custom-violet" />
+                <Star className="h-5 w-5 text-custom-violet fill-custom-violet" />
+                <Star className="h-5 w-5 text-custom-violet fill-custom-violet" />
+                <Star className="h-5 w-5 text-custom-violet fill-custom-violet" />
+                <Star className="h-5 w-5 text-custom-violet fill-custom-violet" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  <span className="p-0.5 bg-slate-800 text-white">Lorem ipsum dolor</span>
+                  sit amet consectetur adipisicing elit. Reprehenderit commodi
+                  consequatur nulla laudantium eius vero voluptatibus
+                  praesentium natus, porro earum cum minus perspiciatis tempore
+                  consequuntur molestiae sint. Laboriosam, sint expedita?
+                </p>
+              </div>
+              <div className='flex gap-4 mt-2'>
+                <img
+                  className='rounded-full h-12 w-12 object-cover'
+                  src='/users/user-1.png'
+                  alt='user'
+                />
+                <div className='flex flex-col'>
+                  <p className='font-semibold'>Jonathan</p>
+                  <div className='flex gap-1.5 items-center text-zinc-600'>
+                    <Check className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <p className='text-sm'>Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+                        {/* second user review */}
+            <div className='flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20'>
+              <div className='flex gap-0.5 mb-2'>
+                <Star className='h-5 w-5 text-custom-violet fill-custom-violet' />
+                <Star className='h-5 w-5 text-custom-violet fill-custom-violet' />
+                <Star className='h-5 w-5 text-custom-violet fill-custom-violet' />
+                <Star className='h-5 w-5 text-custom-violet fill-custom-violet' />
+                <Star className='h-5 w-5 text-custom-violet fill-custom-violet' />
+              </div>
+              <div className='text-lg leading-8'>
+                <p>
+                  I usually keep my phone together with my keys in my pocket
+                  and that led to some pretty heavy scratchmarks on all of my
+                  last phone cases. This one, besides a barely noticeable
+                  scratch on the corner,{' '}
+                  <span className='p-0.5 bg-slate-800 text-white'>
+                    looks brand new after about half a year
+                  </span>
+                  . I dig it.
+                </p>
+              </div>
+              <div className='flex gap-4 mt-2'>
+                <img
+                  className='rounded-full h-12 w-12 object-cover'
+                  src='/users/user-4.jpg'
+                  alt='user'
+                />
+                <div className='flex flex-col'>
+                  <p className='font-semibold'>Josh</p>
+                  <div className='flex gap-1.5 items-center text-zinc-600'>
+                    <Check className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <p className='text-sm'>Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </div>
+  );
+}
